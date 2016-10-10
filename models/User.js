79839cc2a8eec9a1566ce6f9cc1de6363c059b86
Alex Karevoll <alex.karevoll@gmail.com>
2016-10-09 17:03:42 -0700

    path model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose')
 
-var userSchema = mongoose.Scgema({
+var userSchema = mongoose.Schema({
   name : {type: String, required:true},
   email : {type: String, required:true, unique:true},
   password: {type: String},
   img: {type: String},
-  bio: {type: String},
+  bio: {type: Text},
   path: [{type: mongoose.Schema.Types.ObjectID, ref:'Path'}]
 })
 
